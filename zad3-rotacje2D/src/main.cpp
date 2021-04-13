45a218c19979f587f58b4499dc45262a11d8dafb
Olaf Bieganski <olaf.bieganski2001@gmail.com>
2021-04-13 19:29:47 +0200

    gotowe

--- zad3-rotacje2D/src/main.cpp
+++ zad3-rotacje2D/src/main.cpp
@@ -8,11 +8,6 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void wait4key() {
-  do {
-    std::cout << "\n Press a key to continue..." << std::endl;
-  } while(std::cin.get() != '\n');
-}
 
 void porownajBoki(const Prostokat & rectangle){
   Wektor2D bok1, bok2, bok3, bok4;
